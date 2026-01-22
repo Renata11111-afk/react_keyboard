2c50e9d6a23c56ec45be796a4dc69d166bc2ed68
Renata <khotiakovarena@gmail.com>
2026-01-22 19:52:39 +0200

    fixed a mistake

--- src/App.tsx
+++ src/App.tsx
@@ -2,35 +2,39 @@ import React from 'react';
 
 type State = {
   pressedKey: string | null;
-}
+};
 
-export class App extends React.Component<State> {
+export class App extends React.Component<{}, State> {
   state: State = {
     pressedKey: null, //useState(null) [pressedKey, ..]
-  }
+  };
 
   handleKey = (event: KeyboardEvent) => {
     this.setState({ pressedKey: event.key }); //setpressedKey(event.key)
   };
 
   componentDidMount(): void {
-    document.addEventListener('keyup', this.handleKey)
-  };
+    document.addEventListener('keyup', this.handleKey);
+  }
 
   componentWillUnmount(): void {
-    document.removeEventListener('keyup', this.handleKey)
-  };
+    document.removeEventListener('keyup', this.handleKey);
+  }
 
   render() {
     const { pressedKey } = this.state;
 
     return (
       <div className="App">
-        {pressedKey === null
-        ? (<p className="App__message">Nothing was pressed yet</p>)
-        : (<p className="App__message">The last pressed key is {`[${pressedKey}]`}</p>)
-        }
+        {pressedKey === null ? (
+          <p className="App__message">Nothing was pressed yet</p>
+        ) : (
+          <p className="App__message">
+            The last pressed key is {`[${pressedKey}]`}
+          </p>
+        )}
+        ;
       </div>
-    )
+    );
   }
 }
